a4b593a144d9b75c901d94b3fcdd3962d7d6c7f1
No Body <igormikhilov850@gmail.com>
2022-07-06 22:49:05 +0800

    dropbox configured

--- OrderService/settings/dev.py
+++ OrderService/settings/dev.py
@@ -6,7 +6,10 @@ SECRET_KEY = 'c_-0n*cl_fk066!yj#mmph68fc4il%p!=c58qp6j()u870$a=1'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'OrderService',
+        'USER': 'Test',
+        'PASSWORD': 'test',
+        'HOST': 'localhost',
     }
 }
--- OrderService/settings/prod.py
+++ OrderService/settings/prod.py
@@ -1,6 +1,5 @@
 from .common import *
 import os
-import dj_database_url
 
 DEBUG = False
 
@@ -9,16 +8,17 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['krezer-orderservice.herokuapp.com']
 
 DATABASES = {
-    'default': dj_database_url.config() #reads the DATABASE_URL environment variable, parses the connection and returns dictionary
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_LOGIN'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': 'localhost',
+    }
 }
 
-DEFAULT_FILE_STORAGE = 'api.storages.CustomS3Boto3Storage'
-
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_BUCKET_NAME']
-AWS_DEFAULT_ACL = 'public-read'
-AWS_QUERYSTRING_AUTH = True # Changed from "False"
+DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
+DROPBOX_OAUTH2_TOKEN = os.environ['DROPBOX_TOKEN']
 
 SMSAERO_LOGIN = os.environ['SMSAERO_LOGIN']
 SMSAERO_API_KEY = os.environ['SMSAERO_API_KEY']
