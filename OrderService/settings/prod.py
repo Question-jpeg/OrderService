85ba66e06039ae221511d353987d50ebe0ff32d6
No Body <igormikhilov850@gmail.com>
2022-07-10 16:44:24 +0800

    changed default file storage backend

--- OrderService/settings/prod.py
+++ OrderService/settings/prod.py
@@ -5,7 +5,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['krezer-orderservice.herokuapp.com']
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOST1'], os.environ['ALLOWED_HOST2']]
 
 DATABASES = {
     'default': {
@@ -17,8 +17,8 @@ DATABASES = {
     }
 }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
-DROPBOX_OAUTH2_TOKEN = os.environ['DROPBOX_TOKEN']
+DEFAULT_FILE_STORAGE = 'storages.backends.ftp.FTPStorage'
+FTP_STORAGE_LOCATION = os.environ['FTP_STORAGE_LOCATION']
 
 SMSAERO_LOGIN = os.environ['SMSAERO_LOGIN']
 SMSAERO_API_KEY = os.environ['SMSAERO_API_KEY']
