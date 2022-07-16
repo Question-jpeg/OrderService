338b4439259e8ff52608c6bb674edd46e796052d
No Body <igormikhilov850@gmail.com>
2022-07-17 03:50:43 +0800

    send push message fix

--- api/utils/pushNotifications.py
+++ api/utils/pushNotifications.py
@@ -10,7 +10,7 @@ from requests.exceptions import ConnectionError, HTTPError
 
 # Basic arguments. You should extend this function with the push features you
 # want to use, or simply pass in a `PushMessage` object.
-def send_push_message(token, title, body, extra=None):
+def send_push_message(token, title, body, extra={"_displayInForeground": True}):
     response = PushClient().publish(
         PushMessage(to=token,
                     title=title,
